0d185bca4c477bc51877598c62fee684e9831403
Matthew Hallberg <matthewhallberg@gmail.com>
2020-08-10 17:59:36 -0400

    added screen touch position

--- Assets/Scripts/CutHole.cs
+++ Assets/Scripts/CutHole.cs
@@ -4,21 +4,53 @@ public class CutHole : MonoBehaviour {
 
     Renderer imageRend;
 
+    float desiredStrength;
+    float SwirlSpeed = .5f;
+    float SwirlAmount = 100f;
+
     void Start() {
         imageRend = GetComponent<Renderer>();
     }
 
+    void Update() {
+        desiredStrength = Mathf.Lerp(desiredStrength, 0, Time.deltaTime * SwirlSpeed);
+        SetSwirlStrength(desiredStrength);
+    }
+
+    void SetSwirlStrength(float val) {
+        imageRend.material.SetFloat("_SwirlStrength", val);
+    }
+
+    public void OnMouseDown() {
+        desiredStrength = SwirlAmount;
+        SetSwirlStrength(SwirlAmount);
+        PositionSwirlToTouch();
+        SwirlSpeed = .5f;
+    }
+
     public void OnMouseDrag() {
-        RaycastHit hit;
+        PositionSwirlToTouch();
+    }
+
+    public void OnMouseUp() {
+        SwirlSpeed = 3f;
+        PositionSwirlToTouch();
+    }
+
+    void PositionSwirlToTouch() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit)) {
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
             //convert hit from world to localspace
             Vector3 objectPos = transform.InverseTransformPoint(hit.point);
-            //remap to shader coords from 0-1 to -.5-.5
+            //remap to shader coords: -.5-.5 to 0-1
             Vector4 shaderPos = Vector4.zero;
             shaderPos.x = objectPos.x + .5f;
             shaderPos.y = objectPos.y + .5f;
             imageRend.material.SetVector("_SwirlCenter", shaderPos);
         }
+    }
+
+    public static float Remap(float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
