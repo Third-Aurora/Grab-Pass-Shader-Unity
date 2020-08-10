431d065f27c0b5361535725fd79b17573b081b5a
Matthew Hallberg <matthewhallberg@gmail.com>
2020-08-10 16:54:32 -0400

    added touch position to swirl.

--- Assets/Scripts/CutHole.cs
+++ Assets/Scripts/CutHole.cs
@@ -4,4 +4,21 @@ public class CutHole : MonoBehaviour {
 
     Renderer imageRend;
 
+    void Start() {
+        imageRend = GetComponent<Renderer>();
+    }
+
+    public void OnMouseDrag() {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            //convert hit from world to localspace
+            Vector3 objectPos = transform.InverseTransformPoint(hit.point);
+            //remap to shader coords from 0-1 to -.5-.5
+            Vector4 shaderPos = Vector4.zero;
+            shaderPos.x = objectPos.x + .5f;
+            shaderPos.y = objectPos.y + .5f;
+            imageRend.material.SetVector("_SwirlCenter", shaderPos);
+        }
+    }
 }
